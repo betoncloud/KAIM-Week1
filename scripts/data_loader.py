b8e9a3a99d1c3013aabc245e76147b3405eb2219
Bethelhem Teka <bet_oncloud@yahoo.com>
2024-12-15 02:02:37 +0300

    Update data_loader.py

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -1,6 +1,9 @@
 import pandas as pd
-from datetime import datetime
+
+
+
 def load_data(file_path):
+    
     """
     Load data from from a .csv file
     Args:
@@ -8,7 +11,5 @@ def load_data(file_path):
     Returns:
         pd.DataFrame: Loaded data.
     """
-    dateparse = lambda dates: [datetime.strptime(d, '%Y-%m-%d %H:%M:%S') for d in dates]
     
-    return pd.read_csv(file_path,parse_dates=['datetime'], date_parser=dateparse)
-     
+    return pd.read_csv(file_path)
