abc17db99e03e45d337773c249b36b84893375a0
Bethelhem Teka <bet_oncloud@yahoo.com>
2024-12-17 09:59:55 +0300

    adding financial indicators- task-02'

--- scripts/data_analyzer.py
+++ scripts/data_analyzer.py
@@ -3,6 +3,8 @@ import pandas
 import matplotlib
 from textblob import TextBlob
 from summa import keywords
+import pandas as pd
+import pytz
 
 def analyze_sentiment(headline):
     analysis = TextBlob(headline)
@@ -21,3 +23,23 @@ def analyze_sentiment(headline):
 
 def extract_keywords(text):
     return keywords.keywords(text)
+
+def convert_mixed_datetime(value):
+    if pd.isna(value):        
+        return None
+    try:
+        dt = pd.to_datetime(value)  # Convert to datetime
+        if dt.tzinfo is None:       # If naive, make it timezone-aware (e.g., UTC)
+            return dt.replace(tzinfo=pytz.UTC)        
+        return dt  # Return as-is if already timezone-aware
+    except Exception as e:       
+        print(f"Error parsing value: {value}, {e}")
+        return None
+    
+def calculate_rsi(data, window=7):
+    delta = data.diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=window).mean()
+    rs = gain / loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
